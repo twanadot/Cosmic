827240c56f0d7806dc2b0192e757bd3a929fcb5e
devraizer <46655802+devraizer@users.noreply.github.com>
2020-05-04 15:09:59 +0200

    Update gift_clubmembership.sql

--- Database/gift_clubmembership.sql
+++ Database/gift_clubmembership.sql
@@ -1,2 +1,2 @@
-INSERT INTO `website_settings` VALUES ('vip_gift_items', 
+INSERT INTO `website_settings` VALUES ('vip_gift_items', NULL);
 INSERT INTO `website_settings` VALUES ('vip_gift_message', 'Thanks for buying club membership!');
