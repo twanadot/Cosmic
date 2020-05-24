70f4cb3976c11809ac5ac6255eb2ca796c692e32
devraizer <46655802+devraizer@users.noreply.github.com>
2020-05-24 17:56:21 +0200

    Update Api.php

--- Cosmic/App/Controllers/Api.php
+++ Cosmic/App/Controllers/Api.php
@@ -117,7 +117,7 @@ class Api
         response()->json(Core::getCurrencys());
     }
 
-    public function version()
+    public static function version()
     {
         $version_cosmic = @file_get_contents("https://raw.githubusercontent.com/devraizer/Cosmic/master/Cosmic/public/version.txt");
         $version = @file_get_contents("version.txt");
